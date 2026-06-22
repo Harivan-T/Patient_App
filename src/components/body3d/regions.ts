d6b39b701f83f5b513a46200ce26b033983f1fc2
unknown <harivantahir@gmail.com>
2026-06-22 10:57:49 +0300

    Remove body3d locale namespace and deleted files

--- src/components/body3d/regions.ts
+++
@@ -1,7 +0,0 @@
-export const REGION_KEYS = [
-  'head', 'neck', 'chest', 'abdomen', 'pelvis',
-  'arm_left', 'arm_right', 'hand_left', 'hand_right',
-  'leg_left', 'leg_right', 'foot_left', 'foot_right',
-] as const;
-
-export type RegionKey = (typeof REGION_KEYS)[number];
